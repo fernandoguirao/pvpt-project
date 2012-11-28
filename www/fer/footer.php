00ac796babc22ff79b7c94399f4be1682d239626
fernandoguirao <fernando@bueninvento.es>
2012-11-28 23:37:09 +0100

    Estructura header-index-footer ordenada y limpia.

--- www/fer/footer.php
+++ www/fer/footer.php
@@ -2,13 +2,60 @@
 
 				<!-- 3. FOOTER -->
 
-				<footer>
-					<div id="footer">
-						<div class="container">
-							<p class="muted credit">
-								Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.
-							</p>
+				<footer class="footer">
+					<div class="container">
+						<div class="row">
+							<div class="span4">
+								<h2>
+									Pivot point
+								</h2>
+							</div>
+							<div class="span4">
+								<div class="foot-contacto">
+									<form>
+										<fieldset>
+											<legend>Contacta con nosotros</legend>
+											<input type="text" id="inputEmail" placeholder="Nombre y apellidos">
+											<input type="text" id="inputEmail" placeholder="Email">
+											<input type="text" id="inputEmail" placeholder="Teléfono">
+											<textarea rows="3" placeholder="Mensaje"></textarea>
+											<label class="checkbox">
+												<input type="checkbox"> Acepto las condiciones de privacidad
+											</label>
+											<button type="submit" class="btn">Enviar</button>
+										</fieldset>
+									</form>
+								</div>
+							</div>
+							<div class="span4">
+								<div class="mapa">
+									<img alt="" src="http://placekitten.com/297/268">
+									<br>
+									<br>
+								</div>
+								<div class="foot-dirección">
+									<address>
+										<strong>Pivot Point Valencia.</strong><br>
+										795 Folsom Ave, Suite 600<br>
+										San Francisco, CA 94107<br>
+										<abbr title="Phone">P:</abbr> (123) 456-7890
+									</address>
+								</div>
+							</div>
 						</div>
+						<ul class="footer-links">
+							<li>
+								<a href="">Un link cualquiera</a>
+							</li>
+							<li class="muted">·</li>
+							<li>
+								<a href="">Otro link cualquiera</a>
+							</li>
+							<li class="muted">·</li>
+							<li>
+								<a href="">Otro link</a>
+							</li>
+						</ul>
 					</div>
 				</footer> <!-- / FIN DE FOOTER -->
 
