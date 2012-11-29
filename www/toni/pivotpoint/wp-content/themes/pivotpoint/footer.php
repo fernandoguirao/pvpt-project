bcfd1b4f2ff4227429c824d4f5678898fe0aa614
antoniespinosa <antoniespinosa@me.com>
2012-11-29 11:26:57 +0100

    Actualizado 11:10 con solución de errores y mejora de codigo

--- www/toni/pivotpoint/wp-content/themes/pivotpoint/footer.php
+++ www/toni/pivotpoint/wp-content/themes/pivotpoint/footer.php
@@ -5,7 +5,7 @@
  * Contains the closing of the id=main div and all content after
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
+ * @subpackage PivotPoint
  * @since Twenty Eleven 1.0
  */
 ?>
@@ -13,14 +13,6 @@
 	</div><!-- #main -->
 
             <hr>
-            
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				if ( ! is_404() )
-					//get_sidebar( 'footer' );
-			?>
 
             <footer>
                 <p>&copy; Company 2012</p>
@@ -29,7 +21,7 @@
         </div> <!-- /container -->
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
 
         <script src="<?php bloginfo('template_url'); ?>/js/vendor/bootstrap.min.js"></script>
 
