649b1b4a011e70d7b468c250798e130f76d6e6b9
Fernando Guirao <fernando@bueninvento.es>
2012-12-05 19:27:15 +0100

    Probando git mysql

---
+++ databasefer.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 05-12-2012 a las 19:26:33
+-- Versión del servidor: 5.5.9
+-- Versión de PHP: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `pruebagit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tabla git`
+--
+
+CREATE TABLE `tabla git` (
+  `Prueba` varchar(4) NOT NULL DEFAULT '',
+  `pruebagit2` int(11) DEFAULT NULL,
+  `aaaa` varchar(12) DEFAULT NULL,
+  `hhohh` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Prueba`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `tabla git`
+--
+
+INSERT INTO `tabla git` VALUES('2ed', 21, 'hhhbv', NULL);
+INSERT INTO `tabla git` VALUES('aaad', 55, 'scf', 344);
+INSERT INTO `tabla git` VALUES('addd', 33, 'wds', NULL);
+INSERT INTO `tabla git` VALUES('asda', 3, 'NwewULL', NULL);
+INSERT INTO `tabla git` VALUES('ashd', 1, 'wewd', NULL);
+INSERT INTO `tabla git` VALUES('dad', 2, 'NUxwLL', NULL);
+INSERT INTO `tabla git` VALUES('prue', 2, 'nkff', NULL);
+INSERT INTO `tabla git` VALUES('sddd', 222, '4', NULL);
+INSERT INTO `tabla git` VALUES('sdsd', 233, 'asdasd', NULL);
+INSERT INTO `tabla git` VALUES('wcaa', 34, NULL, NULL);
+INSERT INTO `tabla git` VALUES('wdw', 2, 'wx', 5858);
+INSERT INTO `tabla git` VALUES('wdwd', NULL, NULL, NULL);
