aff9053251e6df3ac4dcc0e4bcca4e285289ae0c
Fernando Guirao <fernando@bueninvento.es>
2012-12-07 08:37:52 +0100

    Cambios leves

--- www/fer/calendario.php
+++ www/fer/calendario.php
@@ -2,29 +2,21 @@
 
 				<div id="cuerpo" class="formadores">
 					<div class="sombra hidden-desktop"></div>
-				<div class="container">
-					<div class="captionimg">
-						<h2>
-							Calendario 2012-2013
-						</h2>
+					<div class="mini-header imgbarra">
+						<div class="container">
+							<div class="captionimg">
+								<h2>
+									Calendario 2012-2013
+								</h2>
+							</div>
+						</div> 
+						<div class="imagen-main">
+						</div>
 					</div>
-					</div> 
-
-					<div class="imagen-main"></div>
-					
 					<div id="contenido" class="container">
-<form>
-  <fieldset>
-    <legend>Legend</legend>
-    <label>Label name</label>
-    <input type="text" placeholder="Type something…">
-    <span class="help-block">Example block-level help text here.</span>
-    <label class="checkbox">
-      <input type="checkbox"> Check me out
-    </label>
-    <button type="submit" class="btn">Submit</button>
-  </fieldset>
-</form>
+					
+					<!-- AQUÍ EL CONTENIDO DE LA PÁGINA -->
+					
 					</div>
 				</div>
 
