f1efc90a8581ac8c4327a32fdc3f9ce7fbfd79f3
antoniespinosa <antoniespinosa@me.com>
2012-12-11 13:10:12 +0100

    Fusion estilos

--- www/toni/pivotpoint/wp-content/themes/pivotpoint/index.php
+++ www/toni/pivotpoint/wp-content/themes/pivotpoint/index.php
@@ -1,27 +1,203 @@
-<?php get_header(); ?>
+<?php include ('header.php'); ?>
 
-            <!-- Main hero unit for a primary marketing message or call to action -->
-            <div class="hero-unit">
-                <h1>Hello, world!</h1>
-                <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
-            </div>
+				<!-- 2. CUERPO PRINCIPAL -->
+				
+				<div id="cuerpo" class="index">
+				
+					<!-- 2.1 CAROUSEL -->
+	
+					<div id="myCarousel" class="carousel slide"> 
+	
+						 <!-- 2.1.1 Slides -->
+	
+						 <div class="carousel-inner" >
+							 <div class="active item">
+								<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="100%" />
+								<div class="container">
+									<div class="fix-ie7">
+										<div class="carousel-caption">
+											<h1>Valores únicos para un <strong>espacio único</strong></h1>
+											<p class="lead">
+												Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia.
+											</p>
+											<a class="btn btn-link hidden-phone hidden-tablet" href="#"><i class="icon-book  icon-white"></i>Leer más</a><a class="btn btn-link hidden-phone hidden-tablet" href="#"><i class="icon-share icon-white"></i>Compartir</a>
+											<div class="nav-carousel hidden-phone hidden-tablet">
+												<div class="thumb car-1 activo">
+													<div class="thumb-estado"></div>
+													
+													<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+												
+												</div>
+												<div class="thumb car-2">
+													<div class="thumb-estado"></div>
+													<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+												</div>
+												<div class="thumb car-3">
+													<div class="thumb-estado"></div>
+													<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+												</div>
+												<div style="clear:both;">
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							 <div class="item">
+								<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="100%" />
+								<div class="container">
+									<div class="carousel-caption">
+										<h1>Valores únicos para un <strong>espacio único</strong></h1>
+										<p class="lead">
+											Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia.
+										</p>
+										<a class="btn btn-link" href="#"><i class="icon-book  icon-white"></i>Leer más</a><a class="btn btn-link" href="#"><i class="icon-share icon-white"></i>Compartir</a>
+										<div class="nav-carousel">
+											<div class="thumb car-1">
+												<div class="thumb-estado"></div>
+												
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+												
+											</div>
+											<div class="thumb car-2 activo">
+												<div class="thumb-estado"></div>
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+											</div>
+											<div class="thumb car-3">
+												<div class="thumb-estado"></div>
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+											</div>
+											<div style="clear:both;">
+											</div>
+										</div>
+									 </div>
+								</div>
+							</div>
+							 <div class="item">
+								<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="100%" />
+								<div class="container">
+									<div class="carousel-caption">
+										<h1>Valores únicos para un <strong>espacio único</strong></h1>
+										<p class="lead hidden-phone">
+											Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia.
+										</p>
+										<a class="btn btn-link" href="#"><i class="icon-book  icon-white"></i>Leer más</a><a class="btn btn-link" href="#"><i class="icon-share icon-white"></i>Compartir</a>
+										<div class="nav-carousel">
+											<div class="thumb car-1">
+												<div class="thumb-estado"></div>
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+											</div>
+											<div class="thumb car-2">
+												<div class="thumb-estado"></div>
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+											</div>
+											<div class="thumb car-3 activo">
+												<div class="thumb-estado"></div>
+												<img src="<?php bloginfo('template_url'); ?>/img/contenidos/slide01.jpg" alt="" width="200px" />
+											</div>
+											<div style="clear:both;">
+											</div>
+										</div>
+									 </div>
+								</div>
+							</div>
+						</div>
+					</div> <!-- / FIN DE CAROUSEL -->
+	
+	
+					<div class="container">
+	
+							<!-- 2.2. THUMBS -->
+	
+							<!-- 2.2.1. PRIMERA FILA -->
+	
+							<ul class="thumbnails">
+								<li class="span3">
+									<a href="" class="thumbnail th1">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/img02.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. </p>
+										</div>
+									</a>
+									<a href="" class="thumbnail th5">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/img03.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<div class="thumbnail th9 hidden-phone"></div>
+								</li>
+								<li class="span3">
+									<a href="" class="thumbnail th2">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/foot-img02.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<a href="" class="thumbnail th6">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/foot-img03.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<div class="thumbnail th10 hidden-phone"></div>
+								</li>
+								<li class="span3">
+									<a href="" class="thumbnail th3">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/foot-img03.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<a href="" class="thumbnail th7">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/foot-img02.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<div class="thumbnail th11 hidden-phone"></div>
+								</li>
+								<li class="span3">
+									<a href="" class="thumbnail th4">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/img03.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+									<a href="" class="thumbnail th8">
+										<div class="mascara">
+											<img src="<?php bloginfo('template_url'); ?>/img/sprites/img02.jpg" width="100%" alt="">
+										</div>
+										<div class="texto-th">
+											<h2>Nuestros cursos</h2>
+											<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</div> <!-- / FIN CONTAINER THUMBS -->
+						
+					</div> <!-- / FIN DE CUERPO -->
 
-            <!-- Example row of columns -->
-            <div class="row">
-                <div class="span4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn" href="#">View details &raquo;</a></p>
-                </div>
-                <div class="span4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn" href="#">View details &raquo;</a></p>
-               </div>
-                <div class="span4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <p><a class="btn" href="#">View details &raquo;</a></p>
-                </div>
-<?php get_footer(); ?>
+<?php include ('footer.php'); ?>
